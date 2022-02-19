a1851178420d66f36d9487555809d97e67985341
happygoalvin <yo.alvin.96@gmail.com>
2022-02-20 03:05:35 +0800

    changed map style,added in maori tracks and work on custom markers

--- scripts/map.js
+++ scripts/map.js
@@ -7,29 +7,53 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 20,
     minZoom: 11,
-    id: 'happygoalvin/ckztp5u9m005714lm88jgoqyh',
+    id: 'happygoalvin/ckzu19x7w000915paw31igmdd',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiaGFwcHlnb2FsdmluIiwiYSI6ImNrenJ5djFxZTFlZ3AycnM4eTc2ODJzYWQifQ.5COZ2EO8iEf_uNokeHaNrQ' 
+    accessToken: 'pk.eyJ1IjoiaGFwcHlnb2FsdmluIiwiYSI6ImNrenJ5djFxZTFlZ3AycnM4eTc2ODJzYWQifQ.5COZ2EO8iEf_uNokeHaNrQ'
 }).addTo(map);
 // generate map layer end
+
+// generate custom marker icons start
+// let parkIcon = L.icon({
+//     iconUrl: 'images/park.svg',
+//     iconSize: [32, 37],
+//     iconAnchor: [16, 37],
+//     popupAnchor: [0, -30],
+// })
+
+// generate custom marker icons end
+
+
 
 // load in geoJSON files start
 async function loadParks() {
     let response = await axios.get('data/parks-and-reserves.geojson');
-    let parkLayer = L.geoJson(response.data, {
-        onEachFeature: function(feature,layer){
+    data = response.data;
+    let parkLayer = L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            let parkIcon = L.icon({
+                options: {
+                    iconUrl: 'images/map-icons/park.svg',
+                    iconSize: [32, 37],
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -30],
+                }
+            });
+            return L.marker(latlng, { icon: parkIcon });
+        },
+        onEachFeature: function (feature, layer) {
             layer.bindPopup(feature.properties.name_)
         }
     }).addTo(map);
-    return parkLayer;
-} 
+    return parkLayer
+}
 
 async function loadTracks() {
     let response = await axios.get('data/tracks.geojson');
     let data = response.data
     let trackLayer = L.geoJson(data, {
-        onEachFeature: function(feature,layer){
+        onEachFeature: function (feature, layer) {
             layer.bindPopup(`<div>
             <ul>
             <li>Track Name: ${feature.properties.track_Name}</li>
@@ -38,51 +62,66 @@ async function loadTracks() {
             </div>`)
         }
     }).addTo(map)
-    trackLayer.setStyle ({
-        'color' : 'red'
+    trackLayer.setStyle({
+        'color': 'red'
     })
     return trackLayer;
 }
 
-async function loadCycleRacks(){
+async function loadCycleRacks() {
     let response = await axios.get('data/cycle-racks.geojson')
     let data = response.data
     let cycleRackLayer = L.geoJson(data, {
-        onEachFeature: function(feature, layer){
+        onEachFeature: function (feature, layer) {
             layer.bindPopup(feature.properties.Asset_Search_Description);
         }
     }).addTo(map)
     return cycleRackLayer;
 }
 
-async function loadHeritageTrees(){
+async function loadHeritageTrees() {
     let response = await axios.get('data/heritage-trees.geojson')
     let data = response.data;
-    let heritageTreesLayer = L.geoJson (data, {
-        onEachFeature: function(feature,layer){
+    let heritageTreesLayer = L.geoJson(data, {
+        onEachFeature: function (feature, layer) {
             layer.bindPopup(feature.properties.COMM_NAME)
         }
     }).addTo(map)
     return heritageTreesLayer;
 }
 
-async function loadMaoriSites(){
+async function loadMaoriSites() {
     let response = await axios.get('data/sites-of-maori.geojson')
     let data = response.data;
-    let maoriSitesLayer = L.geoJson (data, {
-        onEachFeature: function(feature, layer){
+    let maoriSitesLayer = L.geoJson(data, {
+        onEachFeature: function (feature, layer) {
             layer.bindPopup(feature.properties.Name)
         }
     }).addTo(map)
     return maoriSitesLayer;
 }
 
-window.addEventListener('DOMContentLoaded', async function(){
+async function loadMaoriTracks() {
+    let response = await axios.get('data/maori-tracks.geojson');
+    let data = response.data
+    let trackLayer = L.geoJson(data, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.Name)
+        }
+    }).addTo(map)
+    trackLayer.setStyle({
+        'color': 'green'
+    })
+    return trackLayer;
+}
+
+window.addEventListener('DOMContentLoaded', async function () {
     loadParks();
     loadTracks();
     loadCycleRacks();
     loadHeritageTrees();
     loadMaoriSites();
+    loadMaoriTracks();
 })
 
 // load in geoJSON files end
