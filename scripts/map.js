5d15b05fbdc69c8bcf5a1c1156c68382459983ae
happygoalvin <yo.alvin.96@gmail.com>
2022-02-21 15:44:42 +0800

    Successfully extracted centre point of parks and created custom Icons for markers and styled tracks

--- scripts/map.js
+++ scripts/map.js
@@ -2,6 +2,7 @@
 let wellington = [-41.28664, 174.77557];
 let map = L.map('map').setView(wellington, 12);
 
+
 // generate map layer start
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -15,15 +16,34 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // generate map layer end
 
 // generate custom marker icons start
-// let parkIcon = L.icon({
-//     iconUrl: 'images/park.svg',
-//     iconSize: [32, 37],
-//     iconAnchor: [16, 37],
-//     popupAnchor: [0, -30],
-// })
+let parkIcon = L.icon({
+    iconUrl: '/images/map-icons/park.svg',
+    iconSize: [27, 27],
+    iconAnchor: [13, 27],
+    popupAnchor: [1, -24],
+})
 
+let treeIcon = L.icon({
+    iconUrl: '/images/map-icons/tree.svg',
+    iconSize: [27, 27],
+    iconAnchor: [13, 27],
+    popupAnchor: [1, -24],
+})
+
+let bicycleIcon = L.icon({
+    iconUrl: '/images/map-icons/bicycle.svg',
+    iconSize: [32, 37],
+    iconAnchor: [16, 37],
+    popupAnchor: [0, -30],
+})
+
+let maoriIcon = L.icon({
+    iconUrl: '/images/map-icons/maori.svg',
+    iconSize: [32, 37],
+    iconAnchor: [16, 37],
+    popupAnchor: [0, -30],
+})
 // generate custom marker icons end
-
 
 
 // load in geoJSON files start
@@ -31,23 +51,30 @@ async function loadParks() {
     let response = await axios.get('data/parks-and-reserves.geojson');
     data = response.data;
     let parkLayer = L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            let parkIcon = L.icon({
-                options: {
-                    iconUrl: 'images/map-icons/park.svg',
-                    iconSize: [32, 37],
-                    iconAnchor: [16, 37],
-                    popupAnchor: [0, -30],
-                }
-            });
-            return L.marker(latlng, { icon: parkIcon });
-        },
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(feature.properties.name_)
+            layer.bindPopup(feature.properties.name_);
         }
     }).addTo(map);
+    parkLayer.setStyle({
+        'color':'#4c8f77'
+    })
     return parkLayer
 }
+
+async function parksCentreMarker() {
+    let response = await axios.get('data/parks-and-reserves.geojson');
+    data = response.data;
+    let parkCentreMarker = L.geoJson(data, {
+        onEachFeature: function(feature,layer) {
+            let lat = layer.getBounds().getCenter()['lat'];
+            let lng = layer.getBounds().getCenter()['lng'];
+            let newMarker = L.marker([lat,lng], {icon: parkIcon});
+            newMarker.bindPopup(feature.properties.name_);
+            newMarker.addTo(map)
+        }
+    })
+    return parkCentreMarker;
+} 
 
 async function loadTracks() {
     let response = await axios.get('data/tracks.geojson');
@@ -63,7 +90,7 @@ async function loadTracks() {
         }
     }).addTo(map)
     trackLayer.setStyle({
-        'color': 'red'
+        'color': '#cc846d'
     })
     return trackLayer;
 }
@@ -73,7 +100,10 @@ async function loadCycleRacks() {
     let data = response.data
     let cycleRackLayer = L.geoJson(data, {
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(feature.properties.Asset_Search_Description);
+            layer.bindPopup(feature.properties.Asset_Search_Description)
+        },
+        pointToLayer: function(feature, latlng){
+            return L.marker(latlng, {icon:bicycleIcon})
         }
     }).addTo(map)
     return cycleRackLayer;
@@ -82,9 +112,12 @@ async function loadCycleRacks() {
 async function loadHeritageTrees() {
     let response = await axios.get('data/heritage-trees.geojson')
     let data = response.data;
-    let heritageTreesLayer = L.geoJson(data, {
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup(feature.properties.COMM_NAME)
+    let heritageTreesLayer = L.geoJson(data,   {
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.COMM_NAME)
+    }, 
+    pointToLayer: function(feature,latlng){
+            return L.marker(latlng, {icon:treeIcon})
         }
     }).addTo(map)
     return heritageTreesLayer;
@@ -96,6 +129,9 @@ async function loadMaoriSites() {
     let maoriSitesLayer = L.geoJson(data, {
         onEachFeature: function (feature, layer) {
             layer.bindPopup(feature.properties.Name)
+        },
+        pointToLayer: function(feature, latlng){
+            return L.marker(latlng, {icon:maoriIcon})
         }
     }).addTo(map)
     return maoriSitesLayer;
@@ -117,6 +153,7 @@ async function loadMaoriTracks() {
 
 window.addEventListener('DOMContentLoaded', async function () {
     loadParks();
+    parksCentreMarker();
     loadTracks();
     loadCycleRacks();
     loadHeritageTrees();
